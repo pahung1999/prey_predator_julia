6ef32c8366faa5b5ea0aedade2ad7bf6dab4dc99
pahung1999 <90453488+pahung1999@users.noreply.github.com>
2022-11-11 17:00:55 +0700

    Add files via upload

--- main.jl
+++ main.jl
@@ -16,4 +16,22 @@ params = Model.ModelParams(
 
 model, agent_step!, model_step! = Model.init_model(params)
 
-abmvideo("./test_v1.mp4", model, agent_step! , model_step!; spf = 1, framerate = 30,frames = 200)
+# abmvideo("./test_v1.mp4", model, agent_step! , model_step!; spf = 1, framerate = 30,frames = 500 ,
+#         heatkwargs = (colormap = [:brown, :green], colorrange = (0, 1)))
+
+function healthy_food(model)
+	count(model.food .> 0.01)
+end
+function IsSpecies(s)
+	function Is(agent)
+		agent.species == s
+	end
+end
+adata = [
+    (IsSpecies(:tiger), count),
+    (IsSpecies(:boar), count),
+    (IsSpecies(:leopard), count)
+]
+adata, mdata = run!(model, agent_step!, model_step!, 100; adata=adata, mdata=[healthy_food])
+
+print(adata)
--- model.jl
+++ model.jl
@@ -42,7 +42,7 @@ end
 
 include("./move_animal.jl")
 include("./eat_prey.jl")
-
+include("./agent_step.jl")
 
 #Hàm tạo loài
 function animal(id, pos, species; energy = rand(0:0.1:1), age = 0)
@@ -98,72 +98,6 @@ function model_step!(model)
 	@. model.food = min(model.food + 0.01, params.max_food)
 end
 
-
-#Step của từng loài
-function agent_step!(agent::Animal, model)
-	params = model.params
-	species = agent.species
-	#Move
-	agent.energy = agent.energy - params.energy_consum[species]
-	if agent.energy <= 0
-		kill_agent!(agent, model)
-		return
-	end
-	agent_step!(agent, model, Val(species))
-
-	#Born
-	if agent.energy > params.energy_reproduce[species] && rand(Uniform(0, 1)) <= params.proba_reproduce[species]
-		nb_offspring= rand(1:MAX_OFFSPRING[species])
-		for _ in 1:nb_offspring
-			id = nextid(model)
-			pos = agent.pos
-			add_agent!(animal(id, pos,species), model)
-		end
-	end
-		
-	# Grow
-	agent.age += 1
-	# Die
-	if agent.age >= params.lifespan[species]
-		kill_agent!(agent, model)
-	end
-end
-
-function agent_step!(agent, model, ::Val{:leopard})
-	# Walk
-	move_animal!(agent,model, Val(agent.species))
-	
-	agent_prey= random_prey(agent.pos , :boar, model)
-	if agent_prey !=0  
-		eat_prey!(agent,agent_prey, model)
-	end
-end
-function agent_step!(agent, model, ::Val{:tiger})
-	# Walk
-	move_animal!(agent,model, Val(agent.species))
-	agent_prey= random_prey(agent.pos , :boar, model)
-	if agent_prey == 0
-		agent_prey= random_prey(agent.pos , :leopard, model)
-	end
-	if agent_prey !=0
-		eat_prey!(agent,agent_prey, model)
-	end
-	
-end
-	
-function agent_step!(agent, model, ::Val{:boar})
-	# Walk
-	move_agent!(agent, rand(nearby_positions(agent.pos, model,1) |> Set), model)
-	
-	params = model.params
-	consumption = min(
-		model.food[agent.pos...], 
-		params.energy_transfert[:boar],
-		params.max_energy[agent.species] - agent.energy
-	)
-	agent.energy += consumption
-	model.food[agent.pos...] -= consumption
-end
 
 function is_species(species)
 	return agent -> agent.species == species
